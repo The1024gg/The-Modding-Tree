eef36b6bce8191db221ea234c994e5f510be96a2
The1024gg <jewelglimpsereble@gmail.com>
2024-10-04 21:28:05 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -11,10 +11,10 @@ addLayer("r", {
         requires:10, // Can be a function that takes requirement increases into account
         resource: "ranks", // Name of prestige currency
 	base: 2,
-        baseResource: "distance", // Name of resource prestige is based on
+        baseResource: "g of mass", // Name of resource prestige is based on
         baseAmount() {return player.points}, // Get the current amount of baseResource
         type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-        exponent: 2, // Prestige currency exponent
+        exponent: 1.5, // Prestige currency exponent
         gainMult() { // Calculate the multiplier for main currency from bonuses
             mult = new Decimal(1)
 
@@ -52,7 +52,7 @@ addLayer("t", {
         baseResource: "ranks", // Name of resource prestige is based on
         baseAmount() {return player.r.points}, // Get the current amount of baseResource
         type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-        exponent: 2, // Prestige currency exponent
+        exponent: 1.5, // Prestige currency exponent
 	base: 1,
         gainMult() { // Calculate the multiplier for main currency from bonuses
             mult = new Decimal(1)
@@ -81,52 +81,52 @@ addLayer("a", {
             rows: 4,
             cols: 4,
             11: {
-                name: "Quick Sprint",
+                name: "Pushing Mass",
                 done() { return player.points.gte(100) },
-                tooltip: "Reach 100 distance",
+                tooltip: "Reach 100g of mass",
             },
 		12: {
-                name: "Better Shoes",
-                done() { return player.r.points.gte(2) },
+                name: "Ranking up",
+                done() { return player.r.points.gte(1) },
                 tooltip: "Rank up",
             },
 		13: {
-                name: "Extreme Workout",
+                name: "Tier out",
                 done() { return player.t.points.gte(1) },
                 tooltip: "Tier up",
             },
 		14: {
-                name: "Off to Space!",
-                done() { return player.ro.points.gte(1) },
-                tooltip: "Rocket Reset",
+                name: "Chapter 2: Rage",
+                done() { return player.rage.points.gte(1) },
+                tooltip: "Rage Reset",
             },
 		21: {
-                name: "Driving for Hours",
-                done() { return player.points.gte(5e5) },
-                tooltip: "Travel 500 km",
+                name: "Mass of Everest",
+                done() { return player.points.gte(1.619e20) },
+                tooltip: "Travel 1 MME",
             },
 		22: {
-                name: "Oil change",
+                name: "Ranking up 2",
                 done() { return player.r.points.gte(8) },
                 tooltip: "Reach rank 8",
             },
 		23: {
-                name: "Three's the Lucky Number",
+                name: "Push up",
                 done() { return player.t.points.gte(3) },
                 tooltip: "Reach tier 3",
             },
 		24: {
-                name: "Blastoff Again?",
-                done() { return player.ro.points.gte(2) },
-                tooltip: "Get 2 rockets",
+                name: "RAGE!!!",
+                done() { return player.rage.points.gte(2) },
+                tooltip: "Get 2 rage powers",
             },
 		31: {
-                name: "Just Under a Saturn Revolution",
-                done() { return player.points.gte(1e12) },
-                tooltip: "Travel 1e12 meters",
+                name: "Earth mass!!!",
+                done() { return player.points.gte(5.97e21) },
+                tooltip: "Reach 1 Earth Mass",
             },
 		32: {
-                name: "Putting in the Fake Fuel",
+                name: "Pushing Mass Simulator",
                 done() { return player.r.points.gte(12) },
                 tooltip: "Reach rank 12",
             },
@@ -136,29 +136,29 @@ addLayer("a", {
                 tooltip: "Reach tier 4",
             },
 		34: {
-                name: "Why fly once when you can fly ten times?",
-                done() { return player.ro.points.gte(10) },
-                tooltip: "Get 10 rockets",
+                name: "Rage III",
+                done() { return player.rage.points.gte(10) },
+                tooltip: "Get 10 rage powers",
             },
 		41: {
-                name: "Parallax Time to the Tenth",
-                done() { return player.points.gte(3.086e17) },
-                tooltip: "Travel 10 parsecs",
+                name: "Solar masses",
+                done() { return player.points.gte(1.98e27) },
+                tooltip: "Push 1 solar masses",
             },
 		42: {
-                name: "Strong Winds",
+                name: "Mass Pusher",
                 done() { return player.r.points.gte(20) },
                 tooltip: "Reach rank 20",
             },
 		43: {
-                name: "Like the drink",
+                name: "Confusion",
                 done() { return player.t.points.gte(5) },
                 tooltip: "Reach tier 5",
             },
 		44: {
-                name: "Now this is just pointless.",
-                done() { return player.ro.points.gte(100000) },
-                tooltip: "Get 1e5 rockets",
+                name: "Mass Pusher v0.1",
+                done() { return player.rage.points.gte(100000) },
+                tooltip: "Get 100,000 rage powers",
             },
         },
         midsection: [
@@ -179,7 +179,7 @@ addLayer("rage", {
 	branches:["t"],
         resource: "rage powers", // Name of prestige currency
 	base: 1,
-        baseResource: "distance", // Name of resource prestige is based on
+        baseResource: "g of mass", // Name of resource prestige is based on
         baseAmount() {return player.points}, // Get the current amount of baseResource
         type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
         exponent: new Decimal(5/24), // Prestige currency exponent
@@ -207,5 +207,49 @@ addLayer("rage", {
 			cost: new Decimal (1)
 		},
 	},
-    }
+}
+})
+
+addLayer("u", {
+        name: "Upgrades", // This is optional, only used in a few places, If absent it just uses the layer id.
+        symbol: "U", // This appears on the layer's node. Default is the id with the first letter capitalized
+        position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+        startData() { return {
+            unlocked: false,
+		points: new Decimal(0),
+        }},
+        color: "#ffffff",
+	base: 1,
+        baseResource: "g of mass", // Name of resource prestige is based on
+        baseAmount() {return player.points}, // Get the current amount of baseResource
+        type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have. custom: You can define everything
+        exponent: new Decimal(5/24), // Prestige currency exponent
+        gainMult() { // Calculate the multiplier for main currency from bonuses
+            mult = new Decimal(1)
+		if (hasAchievement("a",34)) mult=mult.times(1.1)
+		if (hasAchievement("a",44)) mult=mult.times(1.15)
+            return mult
+        },
+        gainExp() { // Calculate the exponent on main currency from bonuses
+		exp=new Decimal(1)	
+            return exp
+        },
+        row: side, // Row the layer is in on the tree (0 is the first row)
+        hotkeys: [
+            {key: "o", description: "Rocket Reset", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        ],
+        layerShown(){return (player.r.points.gte(1)||player.hasAchievement("a",12))},
+	upgrades: {
+		rows: 2,
+		cols: 5,
+		11: {
+			name: "Mass 11",
+			description: "Double mass gain.",
+			currencyDisplayName: "rage powers",
+			currencyInternalName: "rage",
+			currencyLayer: "rage",
+			cost: new Decimal (1)
+		},
+	},
+}
 })
