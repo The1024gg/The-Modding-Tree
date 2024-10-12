c39d134da06900d1083d3346cc73d293ee4c828c
The1024gg <jewelglimpsereble@gmail.com>
2024-10-12 22:44:37 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,17 +1,17 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("adds", {
+    name: "unlocks", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "+", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#808080",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    resource: "unlocks", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
@@ -20,24 +20,9 @@ addLayer("p", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: side, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "u", description: "U: Reset for unlocks", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
-	upgrades: {
-		rows: 3,
-		cols: 4,
-		11: {
-			title: "The Start",
-			description: "Gain 1 point per second.",
-			cost: new Decimal(1)
-		},
-		12: {
-			title: "Boosting",
-			description: "x2 more points.",
-			cost: new Decimal(1),
-			unlocked() { return hasUpgrade("11") },
-		}
-	}
 })
