d41bf5fb211b3a649e6650c18ec8815a37939269
The1024gg <jewelglimpsereble@gmail.com>
2024-10-17 21:44:15 +0800

    Update prestige.js

--- js/prestige.js
+++ js/prestige.js
@@ -13,11 +13,47 @@ addLayer("p", {
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
-    layerShown(){return.player.adds.points.gte(1)},
+    row: 0
+    layerShown(){return player.adds.points.gte(1)},
 	upgrades: {
 		11: {
 			title: "Doubler",
 			description: "x2 point gain",
 			cost: new Decimal(1)
+		},
+		12: {
+			title: "Tripler",
+			description: "x3 point gain",
+			cost: new Decimal(2),
+			unlocked() {return player.adds.points.gte(2)}
+		},
+		13: {
+			title: "Quadrupler",
+			description: "x4 point gain",
+			cost: new Decimal(3),
+			unlocked() {return player.adds.points.gte(2)}
+		},
+		14: {
+            title: "Boosting I",
+            description: "Basic points boost point fragments.",
+            cost: new Decimal(3),
+            effect() {
+                eff = softcap(eff, new Decimal("ee8"), 0.5)
+                eff = softcap(eff, new Decimal("ee50"), 0.4)
+                return eff
+            },
+            effectDisplay() {
+                let softcapDescription = ""
+                let upgEffect = upgradeEffect(this.layer, this.id)
+                if (upgEffect.gte(new Decimal("e100000000")) ) {
+                    softcapDescription = " (softcapped)"
+                }
+                if (upgEffect.gte(new Decimal("e1e50")) ) {
+                    softcapDescription = " (softcapped^2)"
+                }
+                return (upgEffect)+"x" + softcapDescription
+            },
+            unlocked() { return player.adds.poins.gte(2) },
 		}
+	}
 }
