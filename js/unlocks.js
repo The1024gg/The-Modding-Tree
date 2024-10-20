f7acd2656c50cd5f6f4faa154c7c7b2c05845867
The1024gg <jewelglimpsereble@gmail.com>
2024-10-20 19:59:11 +0800

    Update unlocks.js

--- js/unlocks.js
+++ js/unlocks.js
@@ -4,21 +4,21 @@ addLayer("adds", {
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(0),
+		points: new EN(0),
     }},
     color: "#808080",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    requires: new EN(10), // Can be a function that takes requirement increases into account
     resource: "unlocks", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
+        mult = new EN(1)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
+        return new EN(1)
     },
     row: "side", // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
