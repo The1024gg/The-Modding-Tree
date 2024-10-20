7f354b0627bf2d19eeb9b98163dc2f176b635b0b
The1024gg <jewelglimpsereble@gmail.com>
2024-10-20 20:00:13 +0800

    Update prestige.js

--- js/prestige.js
+++ js/prestige.js
@@ -4,10 +4,10 @@ addLayer("p", {
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(0),
+		points: new EN(0),
     }},
     color: "#00FF00",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    requires: new EN(10), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
@@ -19,18 +19,18 @@ addLayer("p", {
 		11: {
 			title: "Doubler",
 			description: "x2 point gain",
-			cost: new Decimal(1)
+			cost: new EN(1)
 		},
 		12: {
 			title: "Tripler",
 			description: "x3 point gain",
-			cost: new Decimal(2),
+			cost: new EN(2),
 			unlocked() {return player.adds.points.gte(2)}
 		},
 		13: {
 			title: "Quadrupler",
 			description: "x4 point gain",
-			cost: new Decimal(3),
+			cost: new EN(3),
 			unlocked() {return player.adds.points.gte(2)}
 		},
 		14: {
